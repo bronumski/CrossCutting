013af17a5364b9c83ac9ebfccc5d7dc1ab3755da
Matt Davis <matt.bronumski@btinternet.com>
2016-02-13 19:30:22 +0000

    Fixing nuget packaging issue

--- src/CrossCutting.Windsor/Properties/AssemblyInfo.cs
+++ src/CrossCutting.Windsor/Properties/AssemblyInfo.cs
@@ -2,6 +2,6 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("CrossCutting.Windsor")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Windsor AOP components")]
 [assembly: ComVisible(false)]
 [assembly: Guid("169278c4-d8f9-4075-97c6-3b66c17672ed")]
